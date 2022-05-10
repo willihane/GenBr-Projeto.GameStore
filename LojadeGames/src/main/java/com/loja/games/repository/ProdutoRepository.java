3e348121025387ec65b42f0a992cf38b569f2fd7
Williane Silva <84298251+willihane@users.noreply.github.com>
2022-05-10 14:50:11 -0300

    Add files via upload

---
+++ LojadeGames/src/main/java/com/loja/games/repository/ProdutoRepository.java
@@ -0,0 +1,16 @@
+package com.loja.games.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.loja.games.model.Produto;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	
+	
+}
